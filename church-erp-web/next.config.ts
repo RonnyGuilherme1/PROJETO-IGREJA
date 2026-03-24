c2e8a3e707833a7064fdd80495617ea9cc0140cf
RonnyGuilherme1 <ronnyguilherme.silva@outlook.com>
2026-03-24 14:24:03 -0300

    v1.5.1 - pequeno ajuste para acessar em outra maquina na mesma rede

--- church-erp-web/next.config.ts
+++ church-erp-web/next.config.ts
@@ -1,14 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.50.41'],
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "http://127.0.0.1:3001/api/:path*",
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:3001/api/:path*',
       },
-    ];
+    ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
